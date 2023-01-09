2bdfeb74fa6d1dfb4cc8dca2a6ae2baacf524b99
Kurteev Dmitry <kurteev.d@yandex.ru>
2023-01-09 07:05:52 +0300

    Missing file

---
+++ distance.js
@@ -0,0 +1,46 @@
+function getDistance(lat1, lon1, lat2, lon2) {
+  if (lat1 == lat2 && lon1 == lon2) {
+    return 0;
+  } else {
+    const radlat1 = (Math.PI * lat1) / 180;
+    const radlat2 = (Math.PI * lat2) / 180;
+    const theta = lon1 - lon2;
+    const radtheta = (Math.PI * theta) / 180;
+
+    let dist =
+      Math.sin(radlat1) * Math.sin(radlat2) +
+      Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+
+    if (dist > 1) {
+      dist = 1;
+    }
+
+    dist = Math.acos(dist);
+    dist = (dist * 180) / Math.PI;
+    dist = dist * 60 * 1.1515;
+
+    return dist * 1.609344 * 1000;
+  }
+}
+
+export default function getNearbyPoints({
+  latitude,
+  longitude,
+  points,
+  distance,
+}) {
+  return points
+    .reduce((points, point) => {
+      const distanceToPoint = getDistance(
+        latitude,
+        longitude,
+        point.latitude,
+        point.longitude
+      );
+
+      return distanceToPoint > distance
+        ? points
+        : [...points, { point, distance: distanceToPoint }];
+    }, [])
+    .sort((pointA, pointB) => pointA.distance - pointB.distance);
+}
