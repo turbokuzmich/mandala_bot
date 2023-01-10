5c818c6f0ac9359346e08c5c6fca7fc1202e0ff7
Kurteev Dmitry <kurteev.d@yandex.ru>
2023-01-10 15:08:37 +0300

    Auth

--- backend.js
+++ backend.js
@@ -265,7 +265,24 @@ const authSchema = {
 };
 
 apiServer.get("/api/web_app", function (request, reply) {
-  reply.code(200), send(request.query);
+  reply.code(200).send(`<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <script src="https://telegram.org/js/telegram-web-app.js"></script>
+  </head>
+  <body>
+    <script>
+      window.addEventListener("load", () => {
+        Telegram.WebApp.showAlert(JSON.stringify("asdasd"));
+      });
+    </script>
+  </body>
+</html>
+  `);
 });
 
 apiServer.get(
